b453df21cde9118e71711b0b1db75a51d49610de
StanleyNdumbe <167863145+StanleyNdumbe@users.noreply.github.com>
2024-11-26 11:44:56 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,4 @@ plugins {
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
     id("com.google.gms.google-services") version "4.4.2" apply false
-}
+}           
